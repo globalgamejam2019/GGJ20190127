d508d5810e2dd8bc45539434818c0d11f0504f6d
ZMKZMK <991301630@qq.com>
2019-01-25 22:01:28 +0800

    最初版本初始化

---
+++ GoHome/Assets/Script/Good/Good.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GoodType
+{
+    Book = 0,
+    Xbox = 1,
+    Switch = 2,
+
+
+
+    Door = 99,
+}
+
+public class Good : MonoBehaviour
+{
+
+
+
+
+
+
+
+
+    #region Public Methons
+    /// <summary>
+    /// 玩家交互物品
+    /// ：player脚本中进行碰撞检测，然后调用此函数
+    /// </summary>
+    /// <param name="goodType">物品类型</param>
+    /// <returns></returns>
+    public int GetInteractiveGood(GoodType goodType)
+    {
+        switch (goodType)
+        {
+                
+        }
+        return 0;
+    }
+
+    #endregion
+}
