b262f914cd2c2b691a1dbb2883ed2d91498a6d7d
ZMKZMK <991301630@qq.com>
2019-01-26 21:07:48 +0800

    两个人物状态机

---
+++ GoHome/Assets/DaCong/playermove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour {
+
+    public float movespeed = 3f;
+    Animator anim;
+    
+    void Start () {
+        anim = GetComponent<Animator>();	
+	}
+    void Update () {
+        if(Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("run",true);
+            transform.Translate(Vector3.right * Time.deltaTime * movespeed);
+        }
+        else
+        {
+            anim.SetBool("run",false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("runleft", true);
+            transform.Translate(Vector3.left * Time.deltaTime * movespeed);
+        }
+        else
+        {
+            anim.SetBool("runleft", false);
+        }
+
+    }
+}
